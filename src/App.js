3d9f3716ffb4aa5d555ad636bc0e0adfa4578e8c
thakurutkarsh22 <thakurutkarsh22@gmail.com>
2021-02-05 01:33:29 +0530

    adding material UI and making skeleton of header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
+    // BEM naming convention.
     <div className="App">
       <h1>Hello lets do slack  :rock</h1>
+      {/* Header */}
+      <Header />
+      {/* Sidebar */}
+      {/* React-Router -> chat screen  */}
 
     </div>
   );
