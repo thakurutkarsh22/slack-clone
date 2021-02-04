99b50a81351086ade81d6284b74818c232d7c2e4
thakurutkarsh22 <thakurutkarsh22@gmail.com>
2021-02-05 02:39:33 +0530

    creating sidebar component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
+import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
   return (
     // BEM naming convention.
     <div className="App">
       <h1>Hello lets do slack  :rock</h1>
-      {/* Header */}
       <Header />
-      {/* Sidebar */}
-      {/* React-Router -> chat screen  */}
+      <div className="app__body">
+        <Sidebar />
+        {/* Sidebar */}
+        {/* React-Router -> chat screen  */}
+      </div>
 
     </div>
   );
---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Sidebar.css';
+import FibreManualRecordIcon from '@material-ui/icons/FiberManualRecord';
+import CreateIcon from '@material-ui/icons/Create';
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <div className="sidebar__header">
+                <div className="sidebar__info">
+                    <h2>Ut Programmer</h2>
+                    <h3>
+                        <FibreManualRecordIcon />
+                        utkarsh thakur
+                    </h3>
+                </div>
+            <CreateIcon />
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
