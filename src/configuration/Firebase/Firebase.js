7eec6d4b835bb1f51a3e8a9a2538e71c9ca0d6c0
thakurutkarsh22 <thakurutkarsh22@gmail.com>
2021-02-06 01:50:28 +0530

    getting sidebar channels from firebase database

--- src/configuration/Firebase/Firebase.js
+++ src/configuration/Firebase/Firebase.js
@@ -13,7 +13,7 @@ const firebaseConfig = {
   };
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
-  const dp = firebaseApp.firestore();
+  const db = firebaseApp.firestore();
   const auth = firebase.auth();
   const provider = new firebase.auth.GoogleAuthProvider();
 
