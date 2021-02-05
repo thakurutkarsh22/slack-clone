baacf3c534b50317a5fa75c373798e116e6342a6
thakurutkarsh22 <thakurutkarsh22@gmail.com>
2021-02-06 02:15:28 +0530

    installing router and routing different compoenent

--- src/App.js
+++ src/App.js
@@ -2,18 +2,29 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     // BEM naming convention.
     <div className="App">
-      <h1>Hello lets do slack  :rock</h1>
+      <Router>
       <Header />
-      <div className="app__body">
-        <Sidebar />
-        {/* Sidebar */}
-        {/* React-Router -> chat screen  */}
-      </div>
+        <div className="app__body">
+          <Sidebar />
+          <Switch>
+            <Route path="/room/:roomId">
+              <h1>Chat Screen</h1>
+              {/* <Chat /> */}
+            </Route>
+            <Route>
+              <h1>Welcome</h1>
+            </Route>
+          </Switch>
+          {/* React-Router -> chat screen  */}
+        </div>
+      </Router>
+      
 
     </div>
   );
