f8de1e6b47e622f5960fe27cc677e242c80d7bf2
thakurutkarsh22 <thakurutkarsh22@gmail.com>
2021-02-06 04:02:28 +0530

    updating room details in chat window

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import './Chat.css';
 import StarBorderOutlinedIcon from '@material-ui/icons/StarBorder';
 import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
+import db from '../../configuration/Firebase/Firebase';
 
 function Chat() {
     const { roomId } = useParams();
+    const [roomDetails, setRoomDetails] = useState(null);
+    const [roomMessages, setRoomMessages] = useState(null);
+
+    useEffect(() => {
+        if(roomId) {
+            db.collection('rooms').doc(roomId).onSnapshot(snapshot => {
+                setRoomDetails(snapshot.data())
+            })
+            db.collection('rooms').doc(roomId)
+            .collection('messages')
+            .orderBy('timestamp', 'asc')
+            .onSnapshot(snapshot => setRoomMessages(snapshot.docs.map(doc => doc.data() ))
+            )
+        }
+    }, [roomId]);
+
     return (
         <div className="chat">
             <div className="chat__header">
                 <div className="chat__headerLeft">
                     <h4 className="chat__channelName">
-                        <strong># general</strong>
+                        <strong>#{roomDetails?.name}</strong>
                         <StarBorderOutlinedIcon />
                     </h4>
                 </div>
@@ -20,6 +37,10 @@ function Chat() {
                         <InfoOutlinedIcon /> Details
                     </p>
                 </div>
+            </div>
+
+            <div className="chat__messages">
+                {/* <Message .../> */}
             </div>
         </div>
     )
