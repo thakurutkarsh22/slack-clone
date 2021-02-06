4b9b09cd6c1bb6f8727aca7671174d833635e049
thakurutkarsh22 <thakurutkarsh22@gmail.com>
2021-02-06 05:37:38 +0530

    creating new Login component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Chat from './components/Chat/Chat';
+import Login from './components/Login/Login';
 
 function App() {
+
+  const [user, setUser] = useState(null);
+
   return (
     // BEM naming convention.
     <div className="app">
       <Router>
-      <Header />
-        <div className="app__body">
-          <Sidebar />
-          <Switch>
-            <Route path="/room/:roomId">
-              <Chat />
-            </Route>
-            <Route>
-              <h1>Welcome</h1>
-            </Route>
-          </Switch>
-          {/* React-Router -> chat screen  */}
-        </div>
+        {!user ? (
+          <Login />
+        ): 
+        (
+          <>
+            <Header />
+              <div className="app__body">
+                <Sidebar />
+                <Switch>
+                  <Route path="/room/:roomId">
+                    <Chat />
+                  </Route>
+                  <Route>
+                    <h1>Welcome</h1>
+                  </Route>
+                </Switch>
+                {/* React-Router -> chat screen  */}
+              </div>
+          </>
+        )}
       </Router>
-      
+    
 
     </div>
   );
---
+++ src/components/Login/Login.js
@@ -0,0 +1,21 @@
+import { Button } from '@material-ui/core';
+import React from 'react';
+import './Login.css';
+
+function Login() {
+    return (
+        <div className="login">
+            <div className="login__container">
+                <img 
+                    src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fs-media-cache-ak0.pinimg.com%2F736x%2F2b%2F26%2F43%2F2b26437d72e949db88e62d251c736c45.jpg&f=1&nofb=1"
+                    alt=""
+                />
+                <h1>Sign in to Bitching HQ</h1>
+                <p>Clover pocket</p>
+                <Button>Sign in with Google</Button>
+            </div>
+        </div>
+    )
+}
+
+export default Login;
