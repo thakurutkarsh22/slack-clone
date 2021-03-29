cde4c4d66837b7129f7ce4f8a2c48163a7534e8e
thakurutkarsh22 <thakurutkarsh22@gmail.com>
2021-03-29 20:32:24 +0530

    configurations firebase

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -30,7 +30,7 @@ function Login() {
                     src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fs-media-cache-ak0.pinimg.com%2F736x%2F2b%2F26%2F43%2F2b26437d72e949db88e62d251c736c45.jpg&f=1&nofb=1"
                     alt=""
                 />
-                <h1>Sign in to Bitching HQ</h1>
+                <h1>Sign in to Almighty HQ</h1>
                 <p>Clover pocket</p>
                 <Button onClick={signIn}>Sign in with Google</Button>
             </div>
